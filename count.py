3b6006b39e001858039aee027ad3f9b51f7e422f
Monsaei <monsaei.delacruz@gmail.com>
2024-03-30 22:34:32 +0800

    Modified count and track

--- count.py
+++ count.py
@@ -1,10 +1,19 @@
-from ultralytics import YOLO
-import numpy as np
 import cv2
-import random
 import pandas as pd
+from ultralytics import YOLO
 from tracker import*
+import cvzone
 
+model=YOLO('weights\yolov8n.pt')
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE :  
+        point = [x, y]
+        print(point)
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+cap=cv2.VideoCapture(r'D:\Documents\Cos\Fourth Year\Second Semester\Final Thesis\DetectTrackCount\videos\traffic1.mp4')
 
 # classes from pretrained model
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
@@ -17,50 +26,47 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
                   "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
                   "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
                   "teddy bear", "hair drier", "toothbrush"
-                  ]
+                ]
 
-# visuals kineme sa bounding boxes
-def RGB(event, x, y, flags, param):
-    if event == cv2.EVENT_MOUSEMOVE :
-        colorsBGR = [x, y]
-        print(colorsBGR)
-
-cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
-
-model = YOLO('../weights/yolov8n.pt', 'v8')
-
-# cap = cv2.VideoCapture(1) //cam
-cap = cv2.VideoCapture("videos/4K_Road_traffic_video_for_object_detection_and_tracking_-_free_download_now.mp4")
 
 if not cap.isOpened():
         print("cannot open camera")
         exit()
 
 count=0
+cy1=424
 
-tracker=Tracker()
+tracker1=Tracker()
+tracker2=Tracker()
+tracker3=Tracker()
 
-cy1=322
-cy2=368
+counter1=[]
+counter2=[]
+counter3=[]
 offset=6
-
 while True:
     ret,frame = cap.read()
     if not ret:
         break
+
     count += 1
     if count % 3 != 0:
         continue
+
     frame=cv2.resize(frame,(1020,500))
+
 
     results=model.predict(frame)
  #   print(results)
     a=results[0].boxes.data
     px=pd.DataFrame(a).astype("float")
 #    print(px)
-    list=[]
-
+    list1=[]
+    motorcycle=[]
+    list2=[]
+    car=[]
+    list3=[]
+    truck=[]
     for index,row in px.iterrows():
 #        print(row)
 
@@ -71,21 +77,32 @@ while True:
         d=int(row[5])
         c=classNames[d]
         if 'car' in c:
-            list.append([x1,y1,x2,y2])
-    bbox_id=tracker.update(list)
-    for bbox in bbox_id:
-        x3,y3,x4,y4,id=bbox
-        cx=int(x3+x4)//2
-        cy=int(y3+y4)//2
-        cv2.circle(frame,(cx,cy),4,(0,0,255),-1)
-        cv2.putText(frame,str(id),(cx,cy),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
+            list1.append([x1,y1,x2,y2])
+            motorcycle.append(c)
+
+    bbox1_idx=tracker1.update(list1)
+
+    for bbox1 in bbox1_idx:
+        for i in motorcycle:
+            x3,y3,x4,y4,id1=bbox1
+            cxm=int(x3+x4)//2
+            cym=int(y3+y4)//2
+            if cym<(cy1+offset) and cym>(cy1-offset):
+               cv2.circle(frame,(cxm,cym),4,(0,255,0),-1)
+               cv2.rectangle(frame,(x3,y3),(x4,y4),(0,0,255),1)
+               cvzone.putTextRect(frame,f'{id1}',(x3,y3),1,1)
+               if counter1.count(id1)==0:
+                  counter1.append(id1)
 
 
+    cv2.line(frame,(2,cy1),(794,cy1),(0,0,255),2)
 
-#    cv2.line(frame,(274,cy1),(814,cy1),(255,255,255),1)
-#    cv2.line(frame,(177,cy2),(927,cy2),(255,255,255),1)
+    motorcyclec=(len(counter1))
+    cvzone.putTextRect(frame,f'motorcyclec:-{motorcyclec}',(19,30),2,1)
+
     cv2.imshow("RGB", frame)
-    if cv2.waitKey(1)&0xFF==27:
+    if cv2.waitKey(0)&0xFF==27:
         break
+
 cap.release()
 cv2.destroyAllWindows()
