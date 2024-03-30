7b5bb5fe3d9cbe47c18e471b4e4df25f1f4ab319
Monsaei <monsaei.delacruz@gmail.com>
2024-03-30 23:12:49 +0800

    Fixed counting of detected objects

--- count.py
+++ count.py
@@ -36,9 +36,12 @@ if not cap.isOpened():
 count=0
 cy1=424
 
+
 tracker1=Tracker()
 tracker2=Tracker()
 tracker3=Tracker()
+
+
 
 counter1=[]
 counter2=[]
@@ -49,10 +52,6 @@ while True:
     if not ret:
         break
 
-    count += 1
-    if count % 3 != 0:
-        continue
-
     frame=cv2.resize(frame,(1020,500))
 
 
@@ -62,11 +61,7 @@ while True:
     px=pd.DataFrame(a).astype("float")
 #    print(px)
     list1=[]
-    motorcycle=[]
-    list2=[]
-    car=[]
-    list3=[]
-    truck=[]
+    vehicle = []
     for index,row in px.iterrows():
 #        print(row)
 
@@ -76,29 +71,32 @@ while True:
         y2=int(row[3])
         d=int(row[5])
         c=classNames[d]
-        if 'car' in c:
+        if 'car'or'bus'or'truck'or'train' in c:
             list1.append([x1,y1,x2,y2])
-            motorcycle.append(c)
+            vehicle.append(c)
 
     bbox1_idx=tracker1.update(list1)
 
     for bbox1 in bbox1_idx:
-        for i in motorcycle:
+        for i in vehicle:
             x3,y3,x4,y4,id1=bbox1
             cxm=int(x3+x4)//2
             cym=int(y3+y4)//2
+            cv2.circle(frame,(cxm,cym),4,(0,0,255),-1)
+            cv2.putText(frame,'Vehicle',(cxm,cym),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2)
             if cym<(cy1+offset) and cym>(cy1-offset):
                cv2.circle(frame,(cxm,cym),4,(0,255,0),-1)
                cv2.rectangle(frame,(x3,y3),(x4,y4),(0,0,255),1)
                cvzone.putTextRect(frame,f'{id1}',(x3,y3),1,1)
                if counter1.count(id1)==0:
                   counter1.append(id1)
-
+   
 
     cv2.line(frame,(2,cy1),(794,cy1),(0,0,255),2)
 
-    motorcyclec=(len(counter1))
-    cvzone.putTextRect(frame,f'motorcyclec:-{motorcyclec}',(19,30),2,1)
+  
+    vehicle=(len(counter1))
+    cvzone.putTextRect(frame,f'vehicle:-{vehicle}',(19,30),2,1)
 
     cv2.imshow("RGB", frame)
     if cv2.waitKey(0)&0xFF==27:
@@ -106,3 +104,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
